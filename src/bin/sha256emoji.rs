2988bb61e87bf123da541aacf9101fef99dc5b8e
Lee Katz <lskatz@gmail.com>
2021-06-13 15:37:04 -0400

    can now do binary

--- src/bin/sha256emoji.rs
+++ src/bin/sha256emoji.rs
@@ -1,13 +1,17 @@
 extern crate clap;
 extern crate emoji;
-extern crate sha256;
+extern crate sha2;
+
+// for opening files
+use std::fs::File;
+use std::io::prelude::*;
 
 //use clap::{Arg, App, SubCommand};
 use clap::{Arg,App,crate_version};
-use sha256::digest_file;
-use std::path::Path;
+use sha2::{Sha256,Digest};
 
 use emoji::lookup_by_glyph::iter_emoji;
+use emoji::Emoji;
 
 fn main() {
     let matches = App::new("sha256emoji")
@@ -19,16 +23,36 @@ fn main() {
                            )
                           .get_matches();
 
+    // Read the input file as binary
     let filename       = matches.value_of("path").unwrap();
-    let input          = Path::new(filename);
-    let digest:String  = digest_file(input).expect("Was not able to hashsum the file");
+    let mut file       = File::open(filename).unwrap();
+    let mut buf:Vec<u8>= Vec::new();
+    file.read_to_end(&mut buf).expect("Could not read the input file to the end");
+
+    // Make a vector of size 32 for a hashsum
+    let mut hasher = Sha256::new();
+    hasher.update(buf);
+    let hashsum_vec = hasher.finalize()
+                      .to_vec();
+
+    // Convert the vector of zero-to-255 numbers to vector of hex
+    let mut hashsum_hex = vec![];
+    for i in hashsum_vec {
+      let hex:String = format!("{:x}", i);
+      hashsum_hex.push(hex);
+    }
+    println!("hashum_hex: {:?}", hashsum_hex.join(""));
+    // make a string
+    let digest_hex:String = hashsum_hex.join("");
+
     /* The full string for the sha256 digest is too large
      * for converting to an int and so we will take a
      * substring of 31 digits. 32 digits seems to parse
      * without overflow and so 31 seems safe.
      */
-    let max_hashsum_digits:usize = 30;
-    let digest_int:u128 = u128::from_str_radix(&digest[0..max_hashsum_digits], 16).unwrap();
+    let max_hashsum_digits:usize = 32;
+    let digest_int:u128 = u128::from_str_radix(&digest_hex[0..max_hashsum_digits], 16).unwrap();
+    println!("digest int: {}", digest_int);
 
     // What percentage of the way are we from zero to max?
     let truncated_max_int:u128 = u128::from_str_radix(&"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"[0..max_hashsum_digits], 16).unwrap();
